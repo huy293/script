27995c3ae1a087f4ca664c9a235dcfbfa3034b81
huy293 <luuhuy.290303@gmail.com>
2025-05-15 00:46:54 +0700

    fix: support puppeteer on render using chrome-aws-lambda

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.post("/comment", async (req, res) => {
     const browser = await puppeteer.launch({
       args: chrome.args,
       defaultViewport: chrome.defaultViewport,
-      executablePath: await chrome.executablePath,
-      headless: chrome.headless,
+      executablePath: await chrome.executablePath || '/usr/bin/google-chrome',
+      headless: chrome.headless
     });
 
     const page = await browser.newPage();
@@ -28,17 +28,19 @@ app.post("/comment", async (req, res) => {
 
     await Promise.all([
       page.click("button[type='submit']"),
-      page.waitForNavigation({ waitUntil: "networkidle0" }),
+      page.waitForNavigation({ waitUntil: "networkidle0" })
     ]);
 
     await browser.close();
 
     return res.json({ status: "Success", message: "Commented successfully" });
   } catch (err) {
+    console.error(err);
     return res.status(500).json({ status: "Error", message: err.message });
   }
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server is running");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`✅ Server is running at http://localhost:${port}`);
 });
