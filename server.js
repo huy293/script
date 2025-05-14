5f6340c8d8d886d320fb23545239f0941191248f
huy293 <luuhuy.290303@gmail.com>
2025-05-14 23:40:20 +0700

    a

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
 const chrome = require('chrome-aws-lambda');
 const app = express();
 app.use(cors());
