de491535e680b84129d6fcf1c3bb8b9c6c4f4f91
huy293 <luuhuy.290303@gmail.com>
2025-05-19 08:47:14 +0700

    a

--- services/browser.js
+++ services/browser.js
@@ -1,30 +1,17 @@
 const puppeteer = require('puppeteer-extra');
-
-const chromium = require('chromium');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
 
 async function launchBrowser() {
   try {
     return await puppeteer.launch({
-      executablePath: chromium.path,
       headless: true,
       args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
         '--disable-gpu',
         '--disable-dev-shm-usage',
-        '--no-first-run',
-        '--no-default-browser-check',
-        '--disable-extensions',
-        '--disable-background-networking',
-        '--disable-sync',
-        '--disable-translate',
-        '--hide-scrollbars',
-        '--mute-audio',
       ],
-      
-      timeout: 60000,
-      protocolTimeout: 120000,
     });
   } catch (error) {
     throw new Error(`[browser.js] Lỗi khi khởi tạo browser: ${error.message}`);
